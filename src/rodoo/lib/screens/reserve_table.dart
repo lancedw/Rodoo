d59576b11270645dc4635ba62dd98809a2c42971
lancedw <lancedewaele@icloud.com>
2022-12-09 12:22:19 +0100

    updated reserve table screen to include book button

--- src/rodoo/lib/screens/reserve_table.dart
+++ src/rodoo/lib/screens/reserve_table.dart
@@ -28,63 +28,93 @@ class _ReserveTableState extends State<ReserveTableScreen> {
       ),
       body: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            const SizedBox(width: 0),
+            Column(
+              // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Column(
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    const Text("Who"),
-                    SizedBox(
-                      height: 200,
-                      width: 100,
-                      child: CupertinoPicker(
-                        itemExtent: 32,
-                        onSelectedItemChanged: (int value) {
-                          setState(() {
-                            numberOfAttendees = value;
-                          });
-                        },
-                        children: [1, 2, 3, 4, 5, 6, 7, 8, 9]
-                            .map((text) => Center(child: Text(text.toString())))
-                            .toList(),
-                      ),
+                    Column(
+                      children: [
+                        const Text("Who"),
+                        SizedBox(
+                          height: 200,
+                          width: 100,
+                          child: CupertinoPicker(
+                            itemExtent: 32,
+                            onSelectedItemChanged: (int value) {
+                              setState(() {
+                                numberOfAttendees = value;
+                              });
+                            },
+                            children: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+                                .map((text) => Center(child: Text(text.toString())))
+                                .toList(),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        const Text("When"),
+                        SizedBox(
+                          height: 200,
+                          width: 200,
+                          child: CupertinoPicker(
+                            itemExtent: 32,
+                            onSelectedItemChanged: (int value) {
+                              setState(() {
+                                selectedMeal = value;
+                              });
+                            },
+                            children: ["Breakfast", "Lunch", "Dinner"]
+                                .map((text) => Center(child: Text(text.toString())))
+                                .toList(),
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
-                Column(
-                  children: [
-                    const Text("When"),
-                    SizedBox(
-                      height: 200,
-                      width: 200,
-                      child: CupertinoPicker(
-                        itemExtent: 32,
-                        onSelectedItemChanged: (int value) {
-                          setState(() {
-                            selectedMeal = value;
-                          });
-                        },
-                        children: ["Breakfast", "Lunch", "Dinner"]
-                            .map((text) => Center(child: Text(text.toString())))
-                            .toList(),
-                      ),
+                const SizedBox(height: 50),
+                const Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 20,
+                  ),
+                  child: Image(
+                    image: AssetImage(
+                      "assets/images/resengo.png",
                     ),
-                  ],
+                  ),
                 ),
               ],
             ),
-            const Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: 20,
-              ),
-              child: Image(
-                image: AssetImage(
-                  "assets/images/resengo.png",
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: SizedBox(
+                height: 44,
+                child: TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(kPrimaryColor),
+                  ),
+                  onPressed: () {
+                    //TODO: continue table booking flow -> time of reservation and account authentication
+                  },
+                  child: Text(
+                    "Book!",
+                    style: generalTextStyle.copyWith(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
                 ),
               ),
-            ),
+            )
           ],
         ),
       ),
